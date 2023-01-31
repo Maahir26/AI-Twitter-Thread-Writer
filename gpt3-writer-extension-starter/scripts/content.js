4e6e52ff7f40863a0fe30f1f55b4124e94cab974
Maahir Sidhu <74683635+Maahir26@users.noreply.github.com>
2023-01-30 19:48:35 -0500

    Add files via upload

---
+++ gpt3-writer-extension-starter/scripts/content.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'inject') {
+        const { content } = request;
+
+        console.log(content);
+
+        sendResponse({ status: 'success' });
+    }
+});
